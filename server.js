d8b9ce5c2b047056a723f0e0b2970db68fc419b6
Vikas Chauhan <vikas@rockstarchef.in>
2023-05-17 16:30:53 +0530

    send response to the browser

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('request mode');
+  console.log("req.url=>", req.url, " req.method=>", req.method);
+  // set headers
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello world');
+  res.write('Hello world again!');
+  res.end();
 });
 
 server.listen(8000, 'localhost', () => {
